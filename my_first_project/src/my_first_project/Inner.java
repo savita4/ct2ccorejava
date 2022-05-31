c5f02ddedaa8dd866ef039e2b5b7430321ae7b22
WELCOME <WELCOME@WELCOME-PC>
2022-05-31 10:04:49 +0530

    first commit

--- my_first_project/src/my_first_project/Inner.java
+++ my_first_project/src/my_first_project/Inner.java
@@ -1,14 +1,28 @@
 package my_first_project;
 
-public class Inner {
-
-	public Inner() {
-		// TODO Auto-generated constructor stub
+class OuterDemo{
+	public void show()
+	{
+		System.out.println("Outer");
+		
 	}
+	public class DemoInner{
+		public void display()
+		{
+			System.out.println("DemoInner");
+		}
+	}
+}
 
+public class Inner {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		OuterDemo Out=new OuterDemo();
+		Out.show();
+		OuterDemo.DemoInner In=Out.new DemoInner();//to acces the inner class use the syntax as outerclass.innerclass object=outerclassobject.new innerclass()
+		
+In.display();
 	}
 
 }
