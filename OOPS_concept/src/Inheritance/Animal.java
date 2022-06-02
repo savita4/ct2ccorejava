8023379b6e448f57c259fc47029fbf7fdbd684ae
WELCOME <WELCOME@WELCOME-PC>
2022-06-02 10:52:00 +0530

    First commit

---
+++ OOPS_concept/src/Inheritance/Animal.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Animal {
+		void eat()
+		{
+			System.out.println("eating...");
+		}  
+	} 
+
+
---
+++ OOPS_concept/src/Inheritance/Cat.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+	public class Cat extends Animal 
+
+	{
+		void bark()
+		{
+			System.out.println("Barking....");
+		}
+	}
+
+
